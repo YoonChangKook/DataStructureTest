7c03182ca942b1e5d2685cec01234a49d0b41864
YoonchangKook <yoonchang.kook@nts-corp.com>
2019-08-20 18:46:44 +0900

    연결리스트 구현 및 테스트 추가

---
+++ src/main/java/com/example/algorithm/LinkedList.java
@@ -0,0 +1,166 @@
+package com.example.algorithm;
+
+/**
+ * 연결 리스트
+ *
+ * @param <T> 연결 리스트에 저장할 객체의 타입
+ */
+public class LinkedList<T> {
+	private Node<T> head;
+
+	public LinkedList() {
+		this.head = null;
+	}
+
+	/**
+	 * 연결 리스트 중간에 노드 삽입한다.
+	 *
+	 * @param preNode 삽입할 노드의 이전 노드
+	 * @param data 삽입할 노드의 데이터
+	 */
+	public void insert(Node preNode, T data) {
+		Node<T> newNode = new Node<>();
+		newNode.setData(data);
+
+		newNode.setLink(preNode.getLink());
+
+		preNode.setLink(newNode);
+	}
+
+	/**
+	 * 연결 리스트 맨 뒤에 노드를 삽입한다.
+	 *
+	 * @param data 삽입할 노드의 데이터
+	 */
+	public void push(T data) {
+		Node<T> newNode = new Node<>();
+		newNode.setData(data);
+
+		if (this.head == null) {
+			this.head = newNode;
+		} else {
+			Node temp = head;
+
+			// 마지막 노드 찾기
+			while (temp.getLink() != null) {
+				temp = temp.getLink();
+			}
+
+			// 마지막에 삽입
+			temp.setLink(newNode);
+		}
+	}
+
+	/**
+	 * 주어진 데이터와 일치하는 노드를 삭제한다.
+	 *
+	 * @param data 삭제할 노드의 데이터
+	 * @throws IllegalArgumentException 데이터와 일치하는 노드가 없을 때
+	 */
+	public void delete(T data) throws IllegalArgumentException {
+		Node preNode = this.head;
+		Node currentNode = this.head.getLink();
+
+		if (data.equals(preNode.getData())) {
+			// 첫번째 노드가 일치할 때
+			this.head = currentNode;
+			preNode.setLink(null);
+
+			return;
+		} else {
+			while (currentNode != null) {
+				if (data.equals(currentNode.getData())) {
+					// 마지막 노드인 경우
+					if (currentNode.getLink() == null) {
+						preNode.setLink(null);
+					} else {
+						preNode.setLink(currentNode.getLink());
+						currentNode.setLink(null);
+					}
+
+					return;
+				}
+
+				preNode = currentNode;
+				currentNode = currentNode.getLink();
+			}
+		}
+
+		throw new IllegalArgumentException(String.format("The data %s does not exist in the list", data));
+	}
+
+	/**
+	 * 주어진 데이터와 일치하는 노드를 찾아서 반환한다.
+	 *
+	 * @param data 찾을 노드의 데이터
+	 * @return 주어진 데이터와 일치하는 노드
+	 * @throws IllegalArgumentException 데이터와 일치하는 노드가 없을 때
+	 */
+	public Node search(T data) throws IllegalArgumentException {
+		Node currentNode = this.head;
+
+		while (currentNode != null) {
+			if (data.equals(currentNode.getData())) {
+				return currentNode;
+			}
+
+			currentNode = currentNode.getLink();
+		}
+
+		throw new IllegalArgumentException(String.format("The data %s does not exist in the list", data));
+	}
+
+	/**
+	 * 리스트가 비어있는지 여부를 반환한다.
+	 *
+	 * @return 리스트가 비어있으면 true
+	 */
+	public boolean isEmpty() {
+		return this.head == null;
+	}
+
+	/**
+	 * 리스트에 존재하는 모든 노드를 출력한다.
+	 */
+	public void print() {
+		Node currentNode = this.head;
+
+		System.out.print("Linked List: ");
+		while (currentNode != null) {
+			System.out.print(String.format("%s -> ", currentNode.getData()));
+			currentNode = currentNode.getLink();
+		}
+		System.out.println();
+	}
+
+	/**
+	 * 연결 리스트를 구성하는 노드 클래스
+	 *
+	 * @param <T> 노드에 저장할 객체의 타입
+	 */
+	public static class Node<T> {
+		private T data;
+		private Node link;
+
+		public Node() {
+			this.data = null;
+			this.link = null;
+		}
+
+		public T getData() {
+			return data;
+		}
+
+		public void setData(T data) {
+			this.data = data;
+		}
+
+		public Node getLink() {
+			return link;
+		}
+
+		public void setLink(Node link) {
+			this.link = link;
+		}
+	}
+}
---
+++ src/test/java/com/example/algorithm/LinkedListTest.java
@@ -0,0 +1,63 @@
+package com.example.algorithm;
+
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+	private static final int TEST_SIZE = 10;
+	private static final int SEARCH_DATA = 5;
+	private static final int INSERT_DATA = 100;
+
+	@Test
+	public void pushAndDeleteTest() {
+		LinkedList<Integer> linkedList = new LinkedList<>();
+
+		// push
+		IntStream.range(0, TEST_SIZE).forEach(linkedList::push);
+		assertFalse(linkedList.isEmpty());
+
+		linkedList.print();
+
+		// delete
+		IntStream.range(0, TEST_SIZE)
+			.boxed()
+			.sorted(Collections.reverseOrder())
+			.forEach(linkedList::delete);
+		assertTrue(linkedList.isEmpty());
+	}
+
+	@Test
+	public void searchTest() {
+		LinkedList<Integer> linkedList = new LinkedList<>();
+
+		IntStream.range(0, TEST_SIZE).forEach(linkedList::push);
+
+		// search
+		LinkedList.Node node = linkedList.search(SEARCH_DATA);
+		assertEquals(SEARCH_DATA, node.getData());
+	}
+
+	@Test
+	public void insertTest() {
+		LinkedList<Integer> linkedList = new LinkedList<>();
+
+		IntStream.range(0, TEST_SIZE).forEach(linkedList::push);
+
+		// search
+		LinkedList.Node node = linkedList.search(SEARCH_DATA);
+		assertEquals(SEARCH_DATA, node.getData());
+		linkedList.print();
+
+		// insert
+		linkedList.insert(node, INSERT_DATA);
+		linkedList.print();
+
+		// search
+		node = linkedList.search(INSERT_DATA);
+		assertEquals(SEARCH_DATA + 1, node.getLink().getData());
+	}
+}
